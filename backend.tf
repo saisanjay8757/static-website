6da7fa609ab52863de665e73687fb5c49238ba33
Sai Sanjay Sudham <sanjay.sudham@cloud4c.com>
2025-01-24 15:03:34 +0530

    Add your commit message here

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "saisanjay-1 "
+    bucket = "saisanjay-1"
     key = "my-terraform-environment/main"
     region = "ap-south-1"
     dynamodb_table = "Terrafom-lock-db-table"
