f90994e68501afcc06e1a450019a7d76f7a231fd
Sai Sanjay Sudham <sanjay.sudham@cloud4c.com>
2025-01-24 15:45:43 +0530

    test3

--- s3.tf
+++ s3.tf
@@ -15,8 +15,7 @@ resource "aws_s3_bucket_public_access_block" "example" {
   ignore_public_acls      = true
   restrict_public_buckets = false
 }
-/*
-resource "aws_s3_bucket_policy" "example" {
+/*resource "aws_s3_bucket_policy" "example" {
   bucket = aws_s3_bucket.mybucket.id
   policy = jsonencode({
     Version = "2012-10-17"
@@ -30,16 +29,16 @@ resource "aws_s3_bucket_policy" "example" {
     ]
   })
 }
-
 */
-resource "aws_s3_bucket_acl" "example" {
+
+/*resource "aws_s3_bucket_acl" "example" {
   depends_on = [
     aws_s3_bucket_ownership_controls.example,
     aws_s3_bucket_public_access_block.example,
   ]
   bucket = aws_s3_bucket.mybucket.id
   acl    = "public-read"
-}
+}*/
 resource "aws_s3_object" "index" {
   bucket = aws_s3_bucket.mybucket.id
   key = "index.html"
