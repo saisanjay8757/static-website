f871712a9432c891cd1f94b8fc81541ca53c3efe
Sai Sanjay Sudham <sanjay.sudham@cloud4c.com>
2025-01-24 15:31:19 +0530

    test1

--- s3.tf
+++ s3.tf
@@ -5,15 +5,29 @@ resource "aws_s3_bucket" "mybucket" {
 resource "aws_s3_bucket_ownership_controls" "example" {
   bucket = aws_s3_bucket.mybucket.id
   rule {
-    object_ownership = "BucketOwnerPreferred"
+    object_ownership = "BucketOwnerEnforced"
   }
 }
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.mybucket.id
-  block_public_acls       = false
+  block_public_acls       = true
   block_public_policy     = false
-  ignore_public_acls      = false
+  ignore_public_acls      = true
   restrict_public_buckets = false
+}
+resource "aws_s3_bucket_policy" "example" {
+  bucket = aws_s3_bucket.mybucket.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.mybucket.arn}/*"
+      }
+    ]
+  })
 }
 resource "aws_s3_bucket_acl" "example" {
   depends_on = [
